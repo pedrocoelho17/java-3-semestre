e329351ceade6cfb12de6b6f8d3d7474ec3bd037
pedrocoelho17 <161032195+pedrocoelho17@users.noreply.github.com>
2025-04-10 10:57:54 -0300

    Add files via upload

---
+++ NomeIdadeEndereco/src/main/java/com/mycompany/nomeidadeendereco/NomeIdadeEndereco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nomeidadeendereco;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class NomeIdadeEndereco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
